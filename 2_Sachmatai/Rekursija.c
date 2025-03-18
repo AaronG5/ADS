6efe297b9fdb316a21201dd4ea1d84ea2f0c2b67
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-03-19 01:13:55 +0200

    Create `stepInfo` function which outputs the information of the ongoing search

--- 2_Sachmatai/Rekursija.c
+++ 2_Sachmatai/Rekursija.c
@@ -1,46 +1,76 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define SIZE 4
+// TODO: make it print to file
+// TODO: make it check for limit of rook pieces (8)
+
+#define SIZE 7
 #define ROOK_AMOUNT 8
 
 int step = 0;
+int amount = 0;
 
-void recursion(int n, int row, bool usedCol[], int combo[]) {
+void stepInfo(char *buffer, int row, int col, bool deeper, bool isBackTrack) {
+   char depth[SIZE] = "\0";
+   //int len = 0;
+
+   for(int i = 0; i < row - 1; ++i) {
+      int len = strlen(depth);
+      depth[len] = '-';
+      depth[len+1] = '\0';
+   }
+
+   sprintf(buffer, "%6d)  %sR%d. Col=%c. Row=%d. %s %s", ++step, depth, col, col + 64, row, (deeper ? "Tinka." : "Netinka."), (isBackTrack ? "Backtrack." : ""));
+
+   printf("\n%s", buffer);
+}
+
+void recursion(int n, int row, bool usedCol[], int combination[], char *buffer) {
    bool deeper;
-   
+   int oldAmount = amount;
+
+
    if(row == n) {
-      printf("End reached!\n");
+      printf("End reached! ");
       for(int i = 0; i < row; ++i) {
-         printf("%d ", combo[i]);
+         printf("%c ", combination[i] + 65);
       }
-      printf("\n");
+      ++amount;
       return;
    }
 
    for(int i = 0; i < n; ++i) {
       deeper = true;
 
+      
       if(usedCol[i]) {
          deeper = false;
       }
 
+      stepInfo(buffer, row + 1, i + 1, deeper, false);
+      
       if(deeper) {
          usedCol[i] = true;
-         combo[row] = i;
-         recursion(n, row+1, usedCol, combo);
+         combination[row] = i;
+         recursion(n, row+1, usedCol, combination, buffer);
          usedCol[i] = false;
       }
+   }
+   if(oldAmount < amount) {
+      printf("Backtrack. ");
    }
 }
 
 int main() {
-   int n = SIZE, row = 0, combo[SIZE];
+   int n = SIZE, row = 0, combination[SIZE];
    bool usedCol[SIZE] = {false};
+   char buffer[100];
 
-   printf("PIRMA DALIS. Duomenys\n  1) Lenta: %dx%d. \n   2) Rikių skaičius: %d.\n", SIZE, SIZE, ROOK_AMOUNT);
+   printf("PIRMA DALIS. Duomenys\n   1) Lenta: %dx%d.\n   2) Rikiu skaicius: %d.\n", SIZE, SIZE, ROOK_AMOUNT);
 
-   printf("ANTRA DALIS. Vykdymas\n");
-   recursion(n, row, usedCol, combo);
-   printf("Why are we here?\n");
+   printf("\nANTRA DALIS. Vykdymas");
+   recursion(n, row, usedCol, combination, buffer);
+
+   printf("\n\nTRECIA DALIS. Rezultatai\n   1) Uzkloti lenta imanoma: %d skirtingais budais.\n   2) Zingsniu prireike: %d.\n", amount, step);
 }
